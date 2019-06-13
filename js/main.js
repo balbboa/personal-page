5a79604b88652b41bec20f1577cc86ed017b8790
balbboa <abmm_arthur@hotmail.com>
2019-06-13 20:50:19 -0300

    some changes

--- js/main.js
+++ js/main.js
@@ -79,10 +79,11 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["[Abzu@localhost ~]$ python whoami.py^2000 \n\n Olá, eu sou Arthur Balboa. Sou pesquisador e desenvolvedor no Laboratório de Inovação Tecnológica em Saúde, LAIS, localizado no Hospital Universitário Onofre Lopes. Também sou graduando do curso de Engenharia de Telecomunicações pela Universidade Federal do Rio Grande do Norte (UFRN).\n\n Usuário Linux e entusiasta da segurança da informação.\n\n Apaixonado por café, música e Niilismo."],
+      strings: ["[Abzu@localhost ~]$ python whoami.py^2000 \n\n Olá, eu sou Arthur Balboa. Sou pesquisador e desenvolvedor no Laboratório de Inovação Tecnológica em Saúde, LAIS, localizado no Hospital Universitário Onofre Lopes. Também sou graduando do curso de Engenharia de Telecomunicações pela Universidade Federal do Rio Grande do Norte (UFRN).\n\n Usuário Linux, amante do Front-end e entusiasta da segurança da informação.\n\n Louco por café e música."],
       typeSpeed: 0,
       loop: false,
-    });
+    }
+    );
   });
   
   // ========================================================================= //
